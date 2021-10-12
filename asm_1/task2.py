8f65d1bf6f3c23d9d6c55823a1a3afc8b0257f64
jdmeijerhof <meijerhof@strw.leidenuniv.nl>
2021-10-12 23:27:02 +0200

    Added static methods to the class SingleLayerPerceptron. Added a forward pass function that will be called in the training loop.

--- asm_1/task2.py
+++ asm_1/task2.py
@@ -7,6 +7,7 @@ def main():
     train_in, train_out = tools.load_training_set()
     test_in, test_out = tools.load_test_set()
     slp = SingleLayerPerceptron(train_in, train_out, test_in, test_out)
+    slp.train_network()
 
 
 class SingleLayerPerceptron:
@@ -22,25 +23,63 @@ class SingleLayerPerceptron:
         assert len(test_entries_inputs) == len(test_entries_outputs)
         assert training_entries_inputs.shape[1] == test_entries_inputs.shape[1]
 
-        self.num_nodes = 10
-        self.num_entries = len(training_entries_inputs)
-        self.num_inputs = len(training_entries_inputs[1])
-
         self.training_input_matrix = self._append_bias_to_input_matrix(training_entries_inputs)
         self.training_outputs = training_entries_output
         self.test_input_matrix = self._append_bias_to_input_matrix(test_entries_inputs)
         self.test_outputs = test_entries_outputs
 
+        self.num_nodes = 10
+        self.num_entries = len(self.training_input_matrix)
+        self.num_inputs = len(self.training_input_matrix[1])
 
-    def _append_bias_to_input_matrix(self, input_matrix):
+        self.W = self._initiate_weights_matrix(self.num_inputs, self.num_nodes)
+
+
+    def train_network(self):
+        self.forward_pass()
+        
+
+    def forward_pass(self):
+        self.current_activations = SingleLayerPerceptron.calculate_activations(
+            self.training_input_matrix, 
+            self.W
+            )
+        self.current_outputs = SingleLayerPerceptron.determine_outputs(
+            self.current_activations
+            )
+
+
+    @staticmethod
+    def calculate_activations(T, W):
+        activation_matrix = np.matmul(T, W)
+        return SingleLayerPerceptron._sigmoid(activation_matrix)
+
+
+    @staticmethod
+    def determine_outputs(A):
+        return np.argmax(A, axis=1)
+
+
+    @staticmethod
+    def _append_bias_to_input_matrix(input_matrix):
         num_entries = input_matrix.shape[0]
         biases = np.ones((num_entries, 1))
         return np.concatenate((input_matrix, biases), axis=1)
 
 
-    def _initiate_weights_matrix(self):
-        pass
+    @staticmethod
+    def _initiate_weights_matrix(num_inputs, num_nodes):
+        return np.random.rand(num_inputs, num_nodes)
 
+
+    @staticmethod
+    def _sigmoid(x):
+        return 1/(1 + np.exp(-x))
+
+
+    @staticmethod
+    def _sigmoid_der(x):
+        return sigmoid(x)*(1-sigmoid(x))
 
 if __name__ == '__main__':
     main()
