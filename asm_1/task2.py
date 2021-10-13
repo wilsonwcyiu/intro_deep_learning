303de18f1e9ca0ca3f7f8200e52ed7d9c12effaf
jdmeijerhof <meijerhof@strw.leidenuniv.nl>
2021-10-13 17:51:01 +0200

    Working implementation of the neural net. Performance is not good yet.

--- asm_1/task2.py
+++ asm_1/task2.py
@@ -1,57 +1,147 @@
+import time
+from matplotlib import pyplot as plt
 import numpy as np
-np.random.seed(42)
+# np.random.seed(42)
 
 import tools
 
 def main():
     train_in, train_out = tools.load_training_set()
     test_in, test_out = tools.load_test_set()
-    slp = SingleLayerPerceptron(train_in, train_out, test_in, test_out)
-    slp.train_network()
+    # learning_rates = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]
+    learning_rates = [0.01]
+    for lr in learning_rates:
+        slp = SingleLayerPerceptron(lr, train_in, train_out, test_in, test_out)
+        slp.train_network()
+        result = slp.classify(test_in)
+        successes = (result == test_out)
+        successful_classification_fraction = np.sum(successes) / len(test_out)
+        print("Test Classification succes = {}%.".format(successful_classification_fraction * 100))
+        slp.plot_precision_sequence()
 
 
 class SingleLayerPerceptron:
 
     def __init__(
         self,
+        learning_rate: float,
         training_entries_inputs: np.ndarray,
-        training_entries_output: np.ndarray,
+        training_entries_outputs: np.ndarray,
         test_entries_inputs: np.ndarray,
         test_entries_outputs: np.ndarray,
     ):
-        assert len(training_entries_inputs) == len(training_entries_output)
+        assert len(training_entries_inputs) == len(training_entries_outputs)
         assert len(test_entries_inputs) == len(test_entries_outputs)
         assert training_entries_inputs.shape[1] == test_entries_inputs.shape[1]
 
-        self.training_input_matrix = self._append_bias_to_input_matrix(training_entries_inputs)
-        self.training_outputs = training_entries_output
-        self.test_input_matrix = self._append_bias_to_input_matrix(test_entries_inputs)
-        self.test_outputs = test_entries_outputs
-
         self.num_nodes = 10
+        self.learning_rate = learning_rate
+        self.training_input_matrix = self._append_bias_to_input_matrix(training_entries_inputs)
+        self.training_desired_outputs = training_entries_outputs
+        self.training_desired_activation_matrix = SingleLayerPerceptron._convert_output_digits_to_desired_node_activation_matrix(training_entries_outputs, self.num_nodes)
+        self.test_input_matrix = self._append_bias_to_input_matrix(test_entries_inputs)
+        self.test_desired_outputs = test_entries_outputs
+        self.test_desired_activation_matrix = SingleLayerPerceptron._convert_output_digits_to_desired_node_activation_matrix(test_entries_outputs, self.num_nodes)
         self.num_entries = len(self.training_input_matrix)
         self.num_inputs = len(self.training_input_matrix[1])
-
         self.W = self._initiate_weights_matrix(self.num_inputs, self.num_nodes)
+        self.training_current_activations = None
+        self.test_current_activations = None
+        self.training_precision = []
+        self.test_precision = []
 
 
-    def train_network(self):
-        self.forward_pass()
-        
-
-    def forward_pass(self):
-        self.current_activations = SingleLayerPerceptron.calculate_activations(
-            self.training_input_matrix, 
-            self.W
+    def train_network(self, training_epochs=5000):
+        print("Training Network")
+        start = time.time()
+        for epoch in range(training_epochs):
+            self.training_current_activations, self.training_output = SingleLayerPerceptron.forward_pass(
+                X = self.training_input_matrix,
+                W = self.W,
             )
-        self.current_outputs = SingleLayerPerceptron.determine_outputs(
-            self.current_activations
+            current_epoch_training_error_matrix, W_updates = SingleLayerPerceptron.backward_pass(
+                learning_rate = self.learning_rate,
+                X = self.training_input_matrix,
+                A = self.training_current_activations,
+                D = self.training_desired_activation_matrix,
             )
+            self.W += W_updates
+            self.training_precision.append(self.calculate_precision_on_training_set())
+            self.test_precision.append(self.calculate_precision_on_test_set())
+        stop = time.time()
+        print("Time elapsed: {:.1f}s".format(stop-start))
+
+
+    def classify(self, input_matrix):
+        input_matrix_with_bias = SingleLayerPerceptron._append_bias_to_input_matrix(input_matrix)
+        A, classifications = SingleLayerPerceptron.forward_pass(input_matrix_with_bias, self.W)
+        return classifications
+
+
+    def calculate_precision_on_training_set(self):
+        successes = (self.training_output == self.training_desired_outputs)
+        return np.sum(successes) / len(self.training_desired_outputs) * 100
+
+
+    def calculate_precision_on_test_set(self):
+        activation_matrix, predictions = SingleLayerPerceptron.forward_pass(
+            X = self.test_input_matrix,
+            W = self.W
+        )
+        successes = (predictions == self.test_desired_outputs)
+        return np.sum(successes) / len(self.test_desired_outputs) * 100
+
+
+    def plot_precision_sequence(self, filename=None):
+        fig, ax = plt.subplots()
+        trained_epochs = len(self.training_precision)
+        ax.plot(range(trained_epochs), self.training_precision, label="Training Set")
+        ax.plot(range(trained_epochs), self.test_precision, label="Test Set")
+        ax.set_xscale('log')
+        ax.set_title("Learning rate: {}".format(self.learning_rate))
+        ax.set_ylabel("Digits classified correctly [%]")
+        ax.set_xlabel("Number of Training Epochs")
+        ax.legend()
+        plt.tight_layout()
+        if filename is not None:
+            plt.savefig(filename)
+            plt.close()
+        else:
+            plt.show()
 
 
     @staticmethod
-    def calculate_activations(T, W):
-        activation_matrix = np.matmul(T, W)
+    def test_convergence(precision_sequence, final_num_epochs):
+        final_epochs = precision_sequence[-final_num_epochs:]
+        if len(precision_sequence) > final_num_epochs:
+            decreasing = all(i < j for i, j in zip(final_epochs, final_epochs[1:]))
+            # similar = (np.std(precision_sequence[-final_num_epochs:]) < np.mean(precision_sequence[-final_num_epochs:]))
+            if decreasing:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+
+    @staticmethod
+    def forward_pass(X, W):
+        A = SingleLayerPerceptron.calculate_activations(X, W)
+        predictions = SingleLayerPerceptron.determine_outputs(A)
+        return A, predictions
+
+
+    @staticmethod
+    def backward_pass(learning_rate, X, A, D):
+        dPHI = SingleLayerPerceptron._sigmoid_der(A)
+        E = D - A
+        dW = learning_rate * np.matmul(X.T, E*dPHI)
+        return E, dW
+
+
+    @staticmethod
+    def calculate_activations(X, W):
+        activation_matrix = np.matmul(X, W)
         return SingleLayerPerceptron._sigmoid(activation_matrix)
 
 
@@ -68,6 +158,18 @@ class SingleLayerPerceptron:
 
 
     @staticmethod
+    def _convert_output_digits_to_desired_node_activation_matrix(output_list, num_nodes):
+        num_entries = len(output_list)
+        desired_activations = np.zeros((num_entries, num_nodes))
+        # The magic slicing term below gives two lists of coordinates to the
+        # array. The first list 'np.arange(num_entries)' denotes we want to
+        # access each row. The second list 'output_list' species the column
+        # index for each row.
+        desired_activations[np.arange(num_entries), output_list] = 1
+        return desired_activations
+
+
+    @staticmethod
     def _initiate_weights_matrix(num_inputs, num_nodes):
         return np.random.rand(num_inputs, num_nodes)
 
@@ -78,8 +180,9 @@ class SingleLayerPerceptron:
 
 
     @staticmethod
-    def _sigmoid_der(x):
-        return sigmoid(x)*(1-sigmoid(x))
+    def _sigmoid_der(sigmoid_x):
+        return sigmoid_x * (1-sigmoid_x)
+
 
 if __name__ == '__main__':
     main()
