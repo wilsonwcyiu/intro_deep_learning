9dfd25c29d63ac0bfb42fec8eab38bb5be72902c
Wilson Yiu <wilson.yiu@mail.digiec.com>
2021-10-15 12:59:51 +0200

    code update

--- asm_1/main/task3c_ver5_lazy_approach.py
+++ asm_1/main/task3c_ver5_lazy_approach.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 # from testCases_v4a import *
 # from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
-
+from matplotlib import pyplot
 
 # Implement the function xor net(x1; x2;weights) that simulates a network with
 # two inputs, two hidden nodes and one output node.
@@ -87,6 +87,22 @@ def xor_net(x1_list, x2_list, weight_vector: list):
     return yhat_list, activation_record_list
 
 
+def plot_multi_list(plot_id: int, plot_title: str, x_label: str, y_label: str, label_data_list_dict: dict):
+    pyplot.figure(plot_id)
+    pyplot.clf()
+
+    pyplot.title(plot_title)
+    pyplot.xlabel(x_label)
+    pyplot.ylabel(y_label)
+
+    for key, item in label_data_list_dict.items():
+        pyplot.plot(item, label=key)
+
+    pyplot.legend()
+
+    # pyplot.pause(0.001)
+
+    return pyplot
 
 
 
@@ -102,53 +118,83 @@ if __name__ == '__main__':
 
     fixed_bias_1: int = 1
 
-    # np.random.seed(42)
-    weight_vector: list = np.random.rand(9, 1)
+    weight_low = -1; weight_high = 1
+    rand_w_1 = np.random.rand(1, 9) * (weight_high - weight_low) + weight_low
 
+    weight_low = -5; weight_high = 5
+    rand_w_2 = np.random.rand(1, 9) * (weight_high - weight_low) + weight_low
+
+    weight_low = -0.1; weight_high = 1
+    rand_w_3 = np.random.rand(1, 9) * (weight_high - weight_low) + weight_low
+
+
+    learning_rate_dict: dict = {"Random weight 1": rand_w_1[0],
+                                "Random weight 2": rand_w_2[0],
+                                "Random weight 3": rand_w_3[0]
+                                }
+
+
+    weight_low: float = -1
+    weight_high: float = 1
+    plot_data_dict: dict = {}
     lr = 0.001
-    idx = 0
-    for epoch in range(200000):
-        yhat_list, activation_record_list = xor_net(x1, x2, weight_vector)
 
-        loss_list: float = []
-        for loss_idx in range(0, len(yhat_list)):
-            loss: float = yhat_list[loss_idx] - all_y_label_list[loss_idx];
-            loss_list.append(loss)
+    for name, random_weight in learning_rate_dict.items():
+        weight_vector = random_weight
 
-        cost = np.sum(loss_list)/ len(loss_list);               #print(cost)
-        mse_value = mse(yhat_list, all_y_label_list)
+        data_list: list = []
+        for epoch in range(2000):
+            yhat_list, activation_record_list = xor_net(x1, x2, weight_vector)
 
-        gradient_desc_vector = grdmse(cost, yhat_list, activation_record_list)
+            loss_list: float = []
+            for loss_idx in range(0, len(yhat_list)):
+                loss: float = yhat_list[loss_idx] - all_y_label_list[loss_idx];
+                loss_list.append(loss)
 
-        for activation_record in activation_record_list:
-            weight_vector[0] -= lr * activation_record[0] * gradient_desc_vector[0]
-            weight_vector[1] -= lr * activation_record[1] * gradient_desc_vector[1]
-            weight_vector[2] -= lr * activation_record[2] * gradient_desc_vector[2]
-            weight_vector[3] -= lr * activation_record[3] * gradient_desc_vector[3]
-            weight_vector[4] -= lr * activation_record[4] * gradient_desc_vector[4]
-            weight_vector[5] -= lr * activation_record[5] * gradient_desc_vector[5]
-            weight_vector[6] -= lr * activation_record[6] * gradient_desc_vector[6]
-            weight_vector[7] -= lr * activation_record[7] * gradient_desc_vector[7]
-            weight_vector[8] -= lr * activation_record[8] * gradient_desc_vector[8]
+            cost = np.sum(loss_list)/ len(loss_list);               #print(cost)
+            mse_value = mse(yhat_list, all_y_label_list)
+
+            gradient_desc_vector = grdmse(cost, yhat_list, activation_record_list)
+
+            for activation_record in activation_record_list:
+                weight_vector[0] -= lr * activation_record[0] * gradient_desc_vector[0]
+                weight_vector[1] -= lr * activation_record[1] * gradient_desc_vector[1]
+                weight_vector[2] -= lr * activation_record[2] * gradient_desc_vector[2]
+                weight_vector[3] -= lr * activation_record[3] * gradient_desc_vector[3]
+                weight_vector[4] -= lr * activation_record[4] * gradient_desc_vector[4]
+                weight_vector[5] -= lr * activation_record[5] * gradient_desc_vector[5]
+                weight_vector[6] -= lr * activation_record[6] * gradient_desc_vector[6]
+                weight_vector[7] -= lr * activation_record[7] * gradient_desc_vector[7]
+                weight_vector[8] -= lr * activation_record[8] * gradient_desc_vector[8]
 
 
 
+            yhat_list = list(map(lambda x: 1 if x > 0.5 else 0, yhat_list))
+            for idx in range(0, len(yhat_list)):
+                if yhat_list[idx] != all_y_label_list[idx]:
+                    misclassification_count += 1
 
+            # misclassification_count_list.append(misclassification_count)
+            # cost_record_list.append(cost)
 
-        yhat_list = list(map(lambda x: 1 if x > 0.5 else 0, yhat_list))
-        for idx in range(0, len(yhat_list)):
-            if yhat_list[idx] != all_y_label_list[idx]:
-                misclassification_count += 1
+            # plot_id = 1
+            # plot_title = "title"
+            # x_label = "Iterations"
+            # y_label = "Cost and misclassification count"
+            # label_data_list_dict: dict = {"Cost": cost_record_list, "Misclassification": misclassification_count_list}
+            if epoch % 1000 == 0:
+                print(str(epoch*4) + "," + str(cost) + "," + str(mse_value) + "," + str(misclassification_count))
+                # plt = plot_multi_list(plot_id, plot_title, x_label, y_label, label_data_list_dict)
+                # plt.show()
 
-        # misclassification_count_list.append(misclassification_count)
-        # cost_record_list.append(cost)
+            data_list.append(cost)
 
-        # plot_id = 1
-        # plot_title = "title"
-        # x_label = "Iterations"
-        # y_label = "Cost and misclassification count"
-        # label_data_list_dict: dict = {"Cost": cost_record_list, "Misclassification": misclassification_count_list}
-        if epoch % 1000 == 0:
-            print(str(epoch*4) + "," + str(cost) + "," + str(mse_value) + "," + str(misclassification_count))
-            # plt = plot_multi_list(plot_id, plot_title, x_label, y_label, label_data_list_dict)
-            # plt.show()
+        plot_data_dict[name] = data_list
+
+    plot_id = 1
+    plot_title = "Cost performance over random weight"
+    x_label = "Iterations"
+    y_label = "Cost"
+    plt = plot_multi_list(plot_id, plot_title, x_label, y_label, plot_data_dict)
+
+    plt.show()
